da8c514a31b08ba6c65516a976234d14af1f2149
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2022-04-16 22:59:53 +0530

    Feat: JS is added to handle onclick event for hamburger menu icon

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function animateMenu() {
+  let dashboard = document.querySelector(".dashboard");
+  dashboard.classList.toggle("grid-width-mobile");
+
+  let aside = document.querySelector(".dash-side");
+  aside.classList.toggle("open");
+}
